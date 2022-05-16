8d3bc53865868c0053aa0724ba92206eb314e31f
[Tom.Cockerill] <tomc37@hotmail.co.uk>
2022-05-16 16:44:41 +0100

    Created select element and populated from JSON

---
+++ libs/php/getCountryNamesFromJSON.php
@@ -0,0 +1,25 @@
+<?php
+
+  	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+	$executionStartTime = microtime(true);
+
+  $result = file_get_contents("..\util\countryBorders.geo.json");
+	$decode = json_decode("$result");
+  $countryNames = array();
+  foreach ($decode->features as $value) {
+    array_push($countryNames, $value->properties->name);
+  };
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $countryNames;
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output);
+
+	?>
