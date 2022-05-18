01a212442cad05c0dfd70b5692860fa8cc0bdfda
[Tom.Cockerill] <tomc37@hotmail.co.uk>
2022-05-18 22:13:22 +0100

    Favicon added

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,173 +1,153 @@
+// Create LeafletJS map
+const map = L.map("map").setView([0, 0], 3);
 
-// Create map
-  const map = L.map("map").setView([0, 0], 3);
+// Create LeafletJS FeatureGroup
+const countryMarkersFeatureGroup = L.featureGroup();
 
-// Create FeatureGroup
-  const countryMarkers = L.featureGroup();
-
-// Load in TileLayer
-  L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-    {
-      minZoom: 2,
-      maxZoom: 10,
-      attribution:
-        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    }
-  ).addTo(map);
-
-// Define Bounds for map
-  const southWest = L.latLng(-89.98155760646617, -180),
-    northEast = L.latLng(89.99346179538875, 180);
-  var bounds = L.latLngBounds(southWest, northEast);
-
-// Set bounds to prevent panning off the map
-  map.setMaxBounds(bounds);
-  map.on("drag", function () {
-    map.panInsideBounds(bounds, { animate: false });
-  });
-
-// Functions
+// Load in LeafletJS TileLayer
+L.tileLayer(
+  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+  {
+    minZoom: 2,
+    maxZoom: 10,
+    attribution:
+      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+  }
+).addTo(map);
 
 // Get coords from current device location
-  const getCoordsFromDeviceLocation = async () => {
-    const pos = await new Promise((resolve, reject) => {
-      window.navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
-    // console.log(pos.coords);
-    return {
-      latitude: pos.coords.latitude,
-      longitude: pos.coords.longitude,
-    };
+const getCoordsFromDeviceLocation = async () => {
+  const pos = await new Promise((resolve, reject) => {
+    window.navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+  return {
+    latitude: pos.coords.latitude,
+    longitude: pos.coords.longitude,
   };
+};
 
 // Ajax Functions
 // Get Country Code from device location
 
-  const getCountryFromLoc = async (coords) => {
-    const countryCode = await new Promise((resolve, reject) => {
-      $.ajax({
-        url: "libs/php/getCountryCodeFromLoc.php",
-        type: "POST",
-        dataType: "json",
-        data: {
-          lat: coords.latitude,
-          lng: coords.longitude,
-        },
-        success: function (result) {
-          if (result.status.name == "ok") {
-            resolve(result.data);
-          }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          console.log(jqXHR);
-          console.log(textStatus);
-          console.log(JSON.stringify(errorThrown));
-          reject(JSON.stringify(errorThrown))
+const getCountryFromLoc = async (coords) => {
+  const countryCode = await new Promise((resolve, reject) => {
+    $.ajax({
+      url: "libs/php/getCountryCodeFromLoc.php",
+      type: "POST",
+      dataType: "json",
+      data: {
+        lat: coords.latitude,
+        lng: coords.longitude,
+      },
+      success: function (result) {
+        if (result.status.name == "ok") {
+          resolve(result.data);
         }
-      })
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(JSON.stringify(errorThrown));
+        reject(JSON.stringify(errorThrown));
+      },
     });
-    return countryCode;
-  };
+  });
+  return countryCode;
+};
 
 // AJAX Functions to parse CountryBorders.geo.json
 
 // Get country borders from countryBorders.geo.json from Country name
-  const getCountryBorderFromCountryName = async (countryName) => {
-    const border = await new Promise((resolve, reject) => {
-      $.ajax({
-        url: "libs/php/getCountryBordersFromJSON.php",
-        type: "POST",
-        dataType: "json",
-        data: {
-          countryName: countryName,
-        },
-        success: function (result) {
-          if (result.status.name == "ok") {
-            resolve(result.data);
-          }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          console.log(jqXHR);
-          console.log(textStatus);
-          console.log(JSON.stringify(errorThrown));
-          reject(JSON.stringify(errorThrown));
-        },
-      });
+const getCountryBorderFromCountryName = async (countryName) => {
+  const border = await new Promise((resolve, reject) => {
+    $.ajax({
+      url: "libs/php/getCountryBordersFromJSON.php",
+      type: "POST",
+      dataType: "json",
+      data: {
+        countryName: countryName,
+      },
+      success: function (result) {
+        if (result.status.name == "ok") {
+          resolve(result.data);
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(JSON.stringify(errorThrown));
+        reject(JSON.stringify(errorThrown));
+      },
     });
-    return border;
-  };
+  });
+  return border;
+};
 
-// Get Country list as array from countryBorders.geo.json and load it into Select
-  const getCountryList = async () => {
-    const countryList = await new Promise((resolve, reject) => {
-      $.ajax({
-        url: "libs/php/getCountryNamesFromJSON.php",
-        type: "GET",
-        dataType: "json",
-        success: function (result) {
-          if (result.status.name == "ok") {
-            resolve(result.data);
-          }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          console.log(jqXHR);
-          console.log(textStatus);
-          console.log(JSON.stringify(errorThrown));
-          reject(JSON.stringify(errorThrown));
-        },
-      })
-    })
-    return countryList;
-  };
+// Get Country list as array from countryBorders.geo.json
+const getCountryList = async () => {
+  const countryList = await new Promise((resolve, reject) => {
+    $.ajax({
+      url: "libs/php/getCountryNamesFromJSON.php",
+      type: "GET",
+      dataType: "json",
+      success: function (result) {
+        if (result.status.name == "ok") {
+          resolve(result.data);
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(JSON.stringify(errorThrown));
+        reject(JSON.stringify(errorThrown));
+      },
+    });
+  });
+  return countryList;
+};
 
-// Define the country border based on geoJSON output from countryBorders.geo.json
-  function createBorder(geoJSON) {
-    countryMarkers.clearLayers();
-    L.geoJSON(geoJSON).addTo(countryMarkers);
-    countryMarkers.addTo(map);
-    map.fitBounds(countryMarkers.getBounds());
-  }
+// Clear featureGroup layers, add geoJSON to featuregroup, add featuregroup to map and fit map bounds to featuregroup bounds
+function createBorder(geoJSON) {
+  countryMarkersFeatureGroup.clearLayers();
+  L.geoJSON(geoJSON).addTo(countryMarkersFeatureGroup);
+  countryMarkersFeatureGroup.addTo(map);
+  map.fitBounds(countryMarkersFeatureGroup.getBounds());
+}
 
-// Function run in document.ready (required separately for async calls)
-  const loaderFunction = async () => {
+// Function to gather all API data needed for Overlay
 
-    // Get list of countries from JSON and populate Select
-    const countryList = await getCountryList();
-    console.log(countryList);
-      countryList.sort();
-    countryList.forEach((country) => {
-      $("#country")
-        .append($('<option>', {value : country})
-        .text(country));
-    })
+// Define single function to run in doc.ready, doc.ready cannot be async and async calls needed.
+const loaderFunction = async () => {
+  // Get list of countries from JSON and populate Select -> Options from list
+  const countryList = await getCountryList();
+  countryList.sort();
+  countryList.forEach((country) => {
+    $("#country").append($("<option>", { value: country }).text(country));
+  });
 
-    // Get lat and long coords from device location
-    const coords = await getCoordsFromDeviceLocation();
-    console.log(coords);
+  // Get lat and long coords from device location
+  const coords = await getCoordsFromDeviceLocation();
 
-    // Get country data from Geonames from coords, languages, countryCode, countryName
-    const countryFromGeoNames = await getCountryFromLoc(coords);
-    console.log(countryFromGeoNames);
+  // Get country data from Geonames from coords, languages, countryCode, countryName
+  const countryFromGeoNames = await getCountryFromLoc(coords);
 
-    // Get border details for Country from JSON and highlight country as selected in Select html element
-    const borderJSON = await getCountryBorderFromCountryName(countryFromGeoNames.countryName);
-    console.log(borderJSON);
-    $("#country").val(countryFromGeoNames.countryName);
+  // Get border details for Country from JSON and highlight country as selected in Select html element
+  const borderJSON = await getCountryBorderFromCountryName(
+    countryFromGeoNames.countryName
+  );
+  $("#country").val(countryFromGeoNames.countryName);
 
-    // Use border details for Country to create polyline on map
-    createBorder(borderJSON);
-
-  };
+  // Use border details for Country to create polyline on map
+  createBorder(borderJSON);
+};
 
 // JQuery Document.Ready function for page load
 $(function () {
   loaderFunction();
 });
 
-$("#country").change(async function() {
+$("#country").change(async function () {
   const countryName = $("#country").val();
   const borderJSON = await getCountryBorderFromCountryName(countryName);
-  console.log(borderJSON);
   createBorder(borderJSON);
-})
+});
